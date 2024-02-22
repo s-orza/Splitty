e5d820a492cc0919fcbe6c2e14fe929ffbb98553
Serban Orza <s.orza@student.tudelft.nl>
2024-02-22 14:21:17 +0100

    checkBoxs logic added

--- client/src/main/java/client/scenes/AddExpenseCtrl.java
+++ client/src/main/java/client/scenes/AddExpenseCtrl.java
@@ -6,6 +6,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddExpenseCtrl {
     @FXML
     private Button addButton;
@@ -26,19 +30,26 @@ public class AddExpenseCtrl {
     private TextField moneyPaid;
 
     @FXML
-    private ComboBox<?> moneyTypeSelector;
+    private ComboBox<String> moneyTypeSelector;
 
     @FXML
     private ComboBox<?> typeSelector;
     @FXML
-    private ListView<String> namesList;
+    private ListView<String> namesList;//names showed on screen from which we select
+    private List<String> selectedNamesList=new ArrayList<>();
     private ObservableList<String> names = FXCollections.observableArrayList(
             "Serban","David","Olav","Alex");
 
     @FXML
     public void initialize() {
+        selectedNamesList=new ArrayList<>();
+        //create the list view with "names"
         namesList.setItems(names);
         namesList.setCellFactory(param -> new CheckBoxListCell());
+        //namesList.getChildren().add(checkBox);   //just an idea for another way of putting names in a list view
+
+        //for handling money type
+        moneyTypeSelector.setOnAction(this::handleCurrencySelection);
     }
     private class CheckBoxListCell extends ListCell<String>{
         private CheckBox checkBox;
@@ -48,7 +59,8 @@ public class AddExpenseCtrl {
             if (empty || item == null) {
                 setGraphic(null);
             } else {
-                checkBox = new CheckBox(item);//we create a checkbox with that name
+                checkBox=new CheckBox(item);//we create a checkbox with that name
+                checkBox.setOnAction(event -> handleCheckBoxSelectName(checkBox));
                 setGraphic(checkBox);
             }
         }
@@ -56,8 +68,8 @@ public class AddExpenseCtrl {
 
     @FXML
     void addExpenseToTheEvent(MouseEvent event) {
-        if(date.getValue()==null)
-        {
+        if(date.getValue()==null) {
+            System.out.println("You need to select date!");
             return;
         }
         int year=date.getValue().getYear();
@@ -66,33 +78,48 @@ public class AddExpenseCtrl {
 
     @FXML
     void cancelAddExpense(MouseEvent event) {
-
+        System.out.println("Expense canceled");
     }
 
     @FXML
     void handleCheckBoxAllPeople(ActionEvent event) {
-        if(checkBoxAllPeople.isSelected())
-        {
+        if(checkBoxAllPeople.isSelected()) {
             System.out.println("Everyone is selected!");
+            //in case the user already selected the "some people" option
+            checkBoxSomePeople.setSelected(false);
         }
-        else
-        {
-            System.out.println("Only some people are selected!");
+        else {
+            System.out.println("everyone options is NOT selected!");
             //show the list of people available
         }
+    }
+    @FXML
+    private void handleCurrencySelection(ActionEvent event) {
+        String selectedMoneyType=moneyTypeSelector.getValue();
+        System.out.println(selectedMoneyType);
     }
 
     @FXML
     void handleCheckBoxSomePeople(ActionEvent event) {
-        if(checkBoxAllPeople.isSelected())
-        {
-            System.out.println("Everyone is selected!");
+        if(checkBoxSomePeople.isSelected()) {
+            namesList.setVisible(true);
+            System.out.println("some people will be selected!");
+            //in case the user already selected the "everyone" option
+            checkBoxAllPeople.setSelected(false);
         }
-        else
-        {
-            System.out.println("Only some people are selected!");
+        else {
+            namesList.setVisible(false);
+            System.out.println("Only some people option is NOT selected!");
             //show the list of people available
         }
-
+    }
+    void handleCheckBoxSelectName(CheckBox checkBox){
+        String name=checkBox.getText();
+        if(checkBox.isSelected()){
+            selectedNamesList.add(name);
+        }
+        else
+            selectedNamesList.remove(name);
+        System.out.println(selectedNamesList);
     }
 }
