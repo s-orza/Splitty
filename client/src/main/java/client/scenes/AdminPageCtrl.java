f14afa6d0bf3bd4e875abdca503be990629521dc
Oliwier Urbański <O.O.Urbanski@student.tudelft.nl>
2024-02-22 13:09:36 +0100

    First commit with basic frontend created

---
+++ client/src/main/java/client/scenes/AdminPageCtrl.java
@@ -0,0 +1,15 @@
+package client.scenes;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AdminPageCtrl extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
