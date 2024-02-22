cc477d749b9eaa689b9e6bc566c5d44bca294695
Serban Orza <s.orza@student.tudelft.nl>
2024-02-22 13:40:22 +0100

    I improved the AddExpenseCtrl with some handles

--- client/src/main/java/client/scenes/AddExpenseCtrl.java
+++ client/src/main/java/client/scenes/AddExpenseCtrl.java
@@ -1,10 +1,24 @@
 package client.scenes;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 public class AddExpenseCtrl {
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private CheckBox checkBoxAllPeople;
+
+    @FXML
+    private CheckBox checkBoxSomePeople;
+
     @FXML
     private DatePicker date;
 
@@ -16,4 +30,69 @@ public class AddExpenseCtrl {
 
     @FXML
     private ComboBox<?> typeSelector;
+    @FXML
+    private ListView<String> namesList;
+    private ObservableList<String> names = FXCollections.observableArrayList(
+            "Serban","David","Olav","Alex");
+
+    @FXML
+    public void initialize() {
+        namesList.setItems(names);
+        namesList.setCellFactory(param -> new CheckBoxListCell());
+    }
+    private class CheckBoxListCell extends ListCell<String>{
+        private CheckBox checkBox;
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item,empty);
+            if (empty || item == null) {
+                setGraphic(null);
+            } else {
+                checkBox = new CheckBox(item);//we create a checkbox with that name
+                setGraphic(checkBox);
+            }
+        }
+    }
+
+    @FXML
+    void addExpenseToTheEvent(MouseEvent event) {
+        if(date.getValue()==null)
+        {
+            return;
+        }
+        int year=date.getValue().getYear();
+        System.out.println(year);
+    }
+
+    @FXML
+    void cancelAddExpense(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleCheckBoxAllPeople(ActionEvent event) {
+        if(checkBoxAllPeople.isSelected())
+        {
+            System.out.println("Everyone is selected!");
+        }
+        else
+        {
+            System.out.println("Only some people are selected!");
+            //show the list of people available
+        }
+    }
+
+    @FXML
+    void handleCheckBoxSomePeople(ActionEvent event) {
+        if(checkBoxAllPeople.isSelected())
+        {
+            System.out.println("Everyone is selected!");
+        }
+        else
+        {
+            System.out.println("Only some people are selected!");
+            //show the list of people available
+        }
+
+    }
 }
