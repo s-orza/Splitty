6990a24f023ad8afddda54e2fcc99f83422d9ff8
Serban Orza <s.orza@student.tudelft.nl>
2024-02-29 19:54:42 +0100

    2 more apis added: find by author and eventId and find only by author

--- server/src/main/java/server/database/ExpenseRepository.java
+++ server/src/main/java/server/database/ExpenseRepository.java
@@ -17,7 +17,12 @@ package server.database;
 
 import commons.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {}
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    @Query("SELECT e FROM Expense e WHERE e.author = :author")
+    List<Expense> findByAuthor(String author);
+}
