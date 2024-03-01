50afb6982b60db7d6d5a449830e5ef4b140ecf6e
Olav Peters <o.c.peters@student.tudelft.nl>
2024-03-01 15:19:09 +0100

    added debt tests

--- commons/src/main/java/commons/Debt.java
+++ commons/src/main/java/commons/Debt.java
@@ -116,6 +116,17 @@ public class Debt {
     }
 
     @Override
+    public String toString() {
+        return "Debt{" +
+                "debtID=" + debtID +
+                ", amount=" + amount +
+                ", currency='" + currency + '\'' +
+                ", debtor=" + debtor +
+                ", creditor=" + creditor +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
---
+++ commons/src/test/java/commons/DebtTest.java
@@ -0,0 +1,57 @@
+package commons;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DebtTest {
+    private double amount;
+    private String currency;
+    private Participant debtor;
+    private Participant creditor;
+
+    @BeforeEach
+    void setUp(){
+        amount = 420.69;
+        currency = "EUR";
+        debtor = new Participant("Steve", "steve@gmail.com", "123", "bic");
+        creditor = new Participant("Alex", "alex@gmail.com", "ABC", "bic2");
+    }
+
+    @Test
+    public void debtConstructorTest(){
+        assertNotNull(new Debt(amount, currency, debtor, creditor));
+    }
+
+    @Test
+    public void amountTest(){
+        Debt debt = new Debt(amount, currency, debtor, creditor);
+        assertEquals(debt.getAmount(), 420.69);
+        debt.setAmount(10.00);
+        assertEquals(debt.getAmount(), 10.00);
+    }
+
+    @Test
+    public void currencyTest(){
+        Debt debt = new Debt(amount, currency, debtor, creditor);
+        assertEquals(debt.getCurrency(), "EUR");
+        debt.setCurrency("USD");
+        assertEquals(debt.getCurrency(), "USD");
+    }
+
+    @Test
+    public void debtorTest(){
+        Debt debt = new Debt(amount, currency, debtor, creditor);
+        assertEquals(debt.getDebtor(), new Participant("Steve", "steve@gmail.com", "123", "bic"));
+        debt.setDebtor(creditor);
+        assertEquals(debt.getDebtor(), new Participant("Alex", "alex@gmail.com", "ABC", "bic2"));
+    }
+
+    @Test
+    public void creditorTest(){
+        Debt debt = new Debt(amount, currency, debtor, creditor);
+        assertEquals(debt.getCreditor(), new Participant("Alex", "alex@gmail.com", "ABC", "bic2"));
+        debt.setCreditor(debtor);
+        assertEquals(debt.getCreditor(), new Participant("Steve", "steve@gmail.com", "123", "bic"));
+    }
+
+}
