3fcc1da1a34221d5048abc6f448df0da8b214ee0
Pop David Gabriel <d.g.pop@student.tudelft.nl>
2024-03-09 14:32:09 +0100

    Added comments for the added methods

--- server/src/main/java/server/api/ParticipantController.java
+++ server/src/main/java/server/api/ParticipantController.java
@@ -16,10 +16,19 @@ import server.database.ParticipantRepository;
 public class ParticipantController {
     private final ParticipantRepository repo;
 
+    /**
+     * Constructor for the ParticipantController
+     * @param repo the repository that holds all participants (of an event)
+     */
     public ParticipantController(ParticipantRepository repo) {
         this.repo = repo;
     }
 
+    /**
+     * Get request for retrieving one participant by ID
+     * @param id the id of the participant to retrieve
+     * @return OK - 200 if the id is valid and participant was found and BAD REQUEST - 400 otherwise
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Participant> getParticipantById(@PathVariable("id") Long id) {
         if (id < 0 || !repo.existsById(id)) {
@@ -28,22 +37,37 @@ public class ParticipantController {
         return ResponseEntity.ok(repo.findById(id).get());
     }
 
+    /**
+     * Get request for all the participants in the repository
+     * @return a response with the list of all participants
+     */
     @GetMapping
     public ResponseEntity<List<Participant>> getAllParticipants() {
         List<Participant> participants = repo.findAll();
         return ResponseEntity.ok(participants);
     }
 
+    /**
+     * Post request to add a participant to an event
+     * @param participant the participant to be added to the
+     * @return
+     */
     @PostMapping(path = { "", "/{eventId}" })
     public ResponseEntity<List<Participant>> createParticipant(@RequestBody Participant participant) {
         List<Participant> participantList = repo.findAll();
         if (!participantList.contains(participant)){
-            participantList.add(participant);
+            repo.saveAndFlush(participant);
         }
 
         return ResponseEntity.ok(repo.findAll());
     }
 
+    /**
+     * Get request for a list of participants given by IDS.
+     * @param ids list of participant ids to search for in the database
+     * @return OK - 200 response if all participants were found, BAD REQUEST - 400 response if at least one participant
+     * is not in the list
+     */
     @GetMapping("/{eventId}/list")
     public ResponseEntity<List<Participant>> getParticipantsByIds(List<Long> ids){
         List<Participant> participantList = new ArrayList<>();
