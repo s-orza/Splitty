ecbb06dc70ae6f91061fba59d98b9a628aaab075
Olav Peters <o.c.peters@student.tudelft.nl>
2024-03-16 04:19:17 +0100

    added settle button

--- client/src/main/java/client/scenes/DebtsCtrl.java
+++ client/src/main/java/client/scenes/DebtsCtrl.java
@@ -7,15 +7,19 @@ import com.google.inject.Injector;
 
 import commons.*;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import javafx.util.Pair;
 
 import javafx.event.ActionEvent;
@@ -46,7 +50,7 @@ public class DebtsCtrl implements Controller, Initializable {
     private TableColumn<Debt, String> amountCol;
 
     @FXML
-    private TableColumn<Debt, String> settleCol;
+    private TableColumn<Debt, Void> settleCol;
 
     @FXML
     private Button cancelButton;
@@ -73,27 +77,76 @@ public class DebtsCtrl implements Controller, Initializable {
 
         refresh();
 
-
         // initialize close button
         cancelButton.setOnAction(this::cancelHandler);
+
+        // render the columns
+        renderCols();
 
         dummyTest();
 
         System.out.println("DebtsCtrl finished initializing");
     }
 
-    private void dummyTest(){
-
-        // set cell factories for columns, receive: (debt)
+    private void renderCols(){
 //        debtorCol.setCellValueFactory(d -> new SimpleStringProperty(server.getParticipant( d.getValue().getDebtor() ).getName()));
 //        creditorCol.setCellValueFactory(d -> new SimpleStringProperty(server.getParticipant( d.getValue().getCreditor() ).getName()));
 //        amountCol.setCellValueFactory(d -> new SimpleStringProperty(Double.toString( d.getValue().getAmount() )));
 //        settleCol.setCellValueFactory(d -> new SimpleStringProperty(Double.toString( d.getValue().getAmount() )));
 
+        // set cell factories for columns, receive: (debt)
         debtorCol.setCellValueFactory(d -> new ReadOnlyStringWrapper(Long.toString(d.getValue().getDebtor())));
         creditorCol.setCellValueFactory(d -> new ReadOnlyStringWrapper(Long.toString(d.getValue().getCreditor())));
         amountCol.setCellValueFactory(d -> new ReadOnlyStringWrapper(Double.toString( d.getValue().getAmount() )));
-        settleCol.setCellValueFactory(d -> new ReadOnlyStringWrapper(Double.toString( d.getValue().getAmount() )));
+        renderSettleCol();
+    }
+
+    private void renderSettleCol(){
+
+        //make a cellFactory for the buttons in Settle Column
+        Callback<TableColumn<Debt, Void>, TableCell<Debt, Void>> cellFactory = new Callback<TableColumn<Debt, Void>, TableCell<Debt, Void>>() {
+            @Override
+            public TableCell<Debt, Void> call(final TableColumn<Debt, Void> param) {
+                final TableCell<Debt, Void> cell = new TableCell<Debt, Void>() {
+
+                    private final Button btn = new Button("Settle");
+
+                    {
+                        //define function of settle button
+                        btn.setOnAction((ActionEvent event) -> {
+                            //retrieve selected debt
+                            Debt debt = getTableView().getItems().get(getIndex());
+                            System.out.println("selectedDebt: " + debt);
+
+                            //settle the debt (currently null since no backend)
+//                            currentDebtManager.settleDebt(debt);
+
+                            //delete and refresh
+                            debtTable.getItems().remove(debt);
+                            refresh();
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        settleCol.setCellFactory(cellFactory);
+
+        debtTable.getColumns().add(settleCol);
+    }
+
+    private void dummyTest(){
 
         // add participants
         Participant anna = new Participant("Anna", "e", "1", "2");
@@ -102,7 +155,7 @@ public class DebtsCtrl implements Controller, Initializable {
 
         anna.setParticipantID(1);
         elsa.setParticipantID(2);
-        elsa.setParticipantID(3);
+        olaf.setParticipantID(3);
 
         // server.addParticipant is not working
 //        server.addParticipant(anna);
@@ -113,6 +166,10 @@ public class DebtsCtrl implements Controller, Initializable {
         Debt d1 = new Debt(10.00, "EUR", anna, elsa);
         Debt d2 = new Debt(69.00, "EUR", anna, olaf);
         Debt d3 = new Debt(5.00, "EUR", olaf, elsa);
+
+        d1.setDebtID(10);
+        d2.setDebtID(20);
+        d3.setDebtID(30);
 
         //put into observable array
         ObservableList<Debt> list = FXCollections.observableArrayList(d1,d2,d3);
