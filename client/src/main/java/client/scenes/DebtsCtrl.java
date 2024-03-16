5da894d1dabd088d54628d3ed2b85b20ea6f6f7a
Olav Peters <o.c.peters@student.tudelft.nl>
2024-03-16 02:09:23 +0100

    added functions

--- client/src/main/java/client/scenes/DebtsCtrl.java
+++ client/src/main/java/client/scenes/DebtsCtrl.java
@@ -4,7 +4,12 @@ import client.MyFXML;
 import client.MyModule;
 import client.utils.ServerUtils;
 import com.google.inject.Injector;
+
+import commons.*;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.util.Pair;
@@ -16,30 +21,28 @@ import javafx.scene.control.TableView;
 
 import javax.inject.Inject;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import static com.google.inject.Guice.createInjector;
 
-public class DebtsCtrl implements Controller{
+public class DebtsCtrl implements Controller, Initializable {
 
 
     @FXML
-    private TableColumn<?, ?> amountCol;
+    private TableView<Debt> debtTable;
+
+    @FXML
+    private TableColumn<Debt, String> debtorCol;
+
+    @FXML
+    private TableColumn<Debt, String> creditorCol;
+
+    @FXML
+    private TableColumn<Debt, String> amountCol;
 
     @FXML
     private Button cancelButton;
-
-    @FXML
-    private TableColumn<?, ?> creditorCol;
-
-    @FXML
-    private TableView<?> debtTable;
-
-    @FXML
-    private TableColumn<?, ?> debtorCol;
-
-    @FXML
-    void close(ActionEvent event) {
-
-    }
 
     //Imports used to swap scenes
     private static final Injector INJECTOR = createInjector(new MyModule());
@@ -49,15 +52,47 @@ public class DebtsCtrl implements Controller{
 
     private Stage stage;
     private ServerUtils server;
+    static DebtManager currentDebtManager;
 
+    // currently constructor injection doesn't work, anywhere
+    // including in EventPageCtrl
     @Inject
-    public DebtsCtrl(ServerUtils server) {
+    public DebtsCtrl(ServerUtils server, long id) {
         this.server = server;
     }
 
     @FXML
-    public void initialize() {}
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("DebtsCtrl initializing");
 
+        refresh();
+
+        // initialize close button
+        cancelButton.setOnAction(this::cancelHandler);
+
+        // set cell factories for columns, receive: (debt)
+        debtorCol.setCellValueFactory(d -> new SimpleStringProperty(server.getParticipant( d.getValue().getDebtor() ).getName()));
+        creditorCol.setCellValueFactory(d -> new SimpleStringProperty(server.getParticipant( d.getValue().getCreditor() ).getName()));
+        creditorCol.setCellValueFactory(d -> new SimpleStringProperty(Double.toString( d.getValue().getAmount() )));
+
+        System.out.println("DebtsCtrl finished initializing");
+    }
+
+    private void cancelHandler(ActionEvent e){
+        System.out.println("closed DebtsCtrl");
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        mainCtrl.initialize(stage, EventPageCtrl.getPair(), EventPageCtrl.getTitle());
+    }
+
+    public void refresh() {
+        System.out.println("refreshed");
+//        // refreshes the Debt Manager and resets the items in the table
+//        var dm = server.getDebtManager();
+//        currentDebtManager = dm;
+//        data = dm.getDebts();
+//        debtTable.setItems(data);
+
+    }
 
     public static Pair<Controller, Parent> getPair() {
         return FXML.load(Controller.class, "client", "scenes", "Debts.fxml");
