1be0025d47b7a1acfde9694c1cb4eabb19ed5fd6
Serban Orza <s.orza@student.tudelft.nl>
2024-03-19 20:36:06 +0100

    The pie chart has the colors of the tags now and the legend is correct. I have also connected it with the event.

--- client/src/main/java/client/scenes/StatisticsCtrl.java
+++ client/src/main/java/client/scenes/StatisticsCtrl.java
@@ -4,10 +4,8 @@ import client.MyFXML;
 import client.MyModule;
 import client.utils.ServerUtils;
 import com.google.inject.Injector;
-import commons.Debt;
 import commons.Expense;
 import commons.Tag;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,14 +15,15 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Pair;
+import javafx.geometry.Pos;
 
 import javax.inject.Inject;
 import java.net.URL;
@@ -33,25 +32,7 @@ import java.util.*;
 import static com.google.inject.Guice.createInjector;
 
 public class StatisticsCtrl implements Controller, Initializable {
-    @javafx.fxml.FXML
-    private TableView<Debt> debtTable;
 
-    @FXML
-    private TableColumn<Debt, String> debtorCol;
-
-    @FXML
-    private TableColumn<Debt, String> creditorCol;
-
-    @FXML
-    private TableColumn<Debt, String> amountCol;
-
-    @FXML
-    private TableColumn<Debt, Void> settleCol;
-
-    @FXML
-    private Button cancelButton;
-
-    //Imports used to swap scenes
     private static final Injector INJECTOR = createInjector(new MyModule());
     private static final MyFXML FXML = new MyFXML(INJECTOR);
 
@@ -71,6 +52,8 @@ public class StatisticsCtrl implements Controller, Initializable {
     @FXML
     private Text totalSpentText;
     @FXML
+    private ListView<String> tagsListView;
+    @FXML
     private ListView<String> legendListView;
     private double totalAmount;
     private List<Expense> expenses;
@@ -82,65 +65,126 @@ public class StatisticsCtrl implements Controller, Initializable {
 
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
-
-        //get all Expenses
-        expenses=server.getAllExpensesOfEvent(7952);
-
         totalAmount=0;
+        //get all Expenses
+        long eventId=7952;
+        if(EventPageCtrl.getCurrentEvent()!=null)
+            eventId=EventPageCtrl.getCurrentEvent().getEventId();
+        expenses=server.getAllExpensesOfEvent(eventId);
+        //get tags with values (each tag with the amount of money it contains.)
+        Map<String, Double> tagsWithValues=getTagsWithValuesFromExpenses(expenses);
+        //create the pieChart
+        createPieChart(tagsWithValues);
+        updateTextsOnTheScreen();
+        //create the list of tags used in this event
+        createTagsUsedInThisEventList(tagsWithValues.keySet());
+    }
+
+    /**
+     * It creates from a list of expenses a map of tag names and total value
+     * of that tag in the event.
+     * @param expensesList the list of expenses
+     * @return a map with tags and total values of the tags.
+     */
+    private Map<String, Double> getTagsWithValuesFromExpenses(List<Expense> expensesList)
+    {
         Map<String, Double> tagsWithValues=new HashMap<>();
-        for(Expense e:expenses)
+        for(Expense e:expensesList)
         {
-            totalAmount+=e.getMoney();
             System.out.println(e.getType()+"  "+e.getMoney());
             if(tagsWithValues.containsKey(e.getType()))
             {
                 double k=tagsWithValues.get(e.getType());
+                //in case the currency is not in EUR, we would change it here.
                 k=k+e.getMoney();
-                tagsWithValues.put(e.getType(), k);
+                tagsWithValues.put(e.getType(),k);
             }
             else
                 tagsWithValues.put(e.getType(),e.getMoney());
         }
-        //we need to change in future from EUR to other currencies
-        totalSpentText.setText("Total sum spent: "+totalAmount+ " EUR");
-        //create the pieChart
-        createPieChart(tagsWithValues);
+        return tagsWithValues;
     }
 
+    /**
+     * Here it create the pie chart and set colors base on the input.
+     * Also, it creates a legend with colors, names and percentages.
+     * This is a good function to call when the page needs to refresh
+     * @param tagsWithValues the input
+     */
     private void createPieChart(Map<String, Double> tagsWithValues)
     {
         ObservableList<PieChart.Data> pieChartData =FXCollections.observableArrayList();
-
+        //calculate the total amount of money
+        totalAmount=0;
+        totalAmount=expenses.stream().mapToDouble(x->x.getMoney()).sum();
+        long eventId=7952;
+        if(EventPageCtrl.getCurrentEvent()!=null)
+            eventId=EventPageCtrl.getCurrentEvent().getEventId();
+        //a map with the name and the color of a tag
         Map<String, String> tagColors = new HashMap<>();
+
         for(String tag: tagsWithValues.keySet()){
             double amount=tagsWithValues.get(tag);
-            double percentage = (amount/ totalAmount) * 100;
-            String name=tag + " (" + String.format("%.2f", percentage) + "%)";
+            double percentage=(amount/totalAmount)*100;
+            //the tag name + %
+            String name=tag+ " ("+String.format("%.2f", percentage)+"%)";
             PieChart.Data data=new PieChart.Data(name,amount);
-            //data.getNode().setStyle("-fx-pie-color: " + "#ffffff" + ";");
-            //put an if to see if it EXISTED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            if(!tagColors.containsKey(name))
-                tagColors.put(name,"#f0ff11");
             pieChartData.add(data);
+            //preparing tagColors to use for personalized colors
+            if(!tagColors.containsKey(name))
+            {
+                Tag t=server.getTagByIdOfEvent(tag,eventId);
+                if(t!=null)
+                    tagColors.put(name,t.getColor());
+                else
+                    tagColors.put(name,"#aaaaaa");
+                //This is a gray
+            }
         }
         pieChart.setData(pieChartData);
-        //add colors
+        //add colors to the pie chart
         pieChartData.forEach(data -> {
             String color = tagColors.get(data.getName());
             data.getNode().setStyle("-fx-pie-color: " + color + ";");
         });
+
         System.out.println(tagColors);
 
+        //create the legend
         legendListView.setCellFactory(param -> new LegendListCell(tagColors));
 
-        // Populate the ListView with legend item names
+        // put data in the legend
         legendListView.getItems().clear();
         legendListView.getItems().addAll(tagColors.keySet());
-
-
-
     }
-    private static class LegendListCell extends ListCell<String> {
+    private void createTagsUsedInThisEventList(Set<String> tags)
+    {
+        tagsListView.getItems().clear();
+        tagsListView.getItems().addAll(tags);
+        tagsListView.setCellFactory(param -> new TagsListViewCell());
+        //we need these for making the front end beautiful
+        tagsListView.setSelectionModel(null);
+        tagsListView.setFocusTraversable(false);
+    }
+
+    /**
+     * Here it updates the total amount text and the title
+     */
+    private void updateTextsOnTheScreen()
+    {
+        //update the name of the event
+        long eventId=7952;
+        if(EventPageCtrl.getCurrentEvent()!=null)
+            eventId=EventPageCtrl.getCurrentEvent().getEventId();
+        titleId.setText("Statistics for event "+eventId);
+        //in case we don't have the total amount in EUR, we need to change it to EUR
+        totalSpentText.setText("Total sum spent: "+totalAmount+ " EUR");
+    }
+
+    /**
+     * Class used for the legend.
+     */
+    private class LegendListCell extends ListCell<String> {
         private final Map<String, String> tagColors;
 
         public LegendListCell(Map<String, String> tagColors) {
@@ -157,14 +201,75 @@ public class StatisticsCtrl implements Controller, Initializable {
             } else {
                 Circle circle = new Circle(6);
                 circle.setFill(Color.web(tagColors.get(item)));
-
-                setGraphic(new HBox(circle, new Label(item)));
+                //for setting the size of the text (by default it is 12)
+                Label label=new Label(item);
+                label.setFont(new Font(13));
+                //we need to align the circle and the text
+                HBox hbox=new HBox(circle, label);
+                hbox.setAlignment(Pos.CENTER_LEFT);
+                hbox.setSpacing(2);
+                setGraphic(hbox);
             }
         }
     }
+    private class TagsListViewCell extends ListCell<String>{
+        private HBox container;
+        private Label tagLabel;
+        private Button editButton;
+        private Button deleteButton;
 
+        private TagsListViewCell() {
+            container=new HBox();
+            tagLabel=new Label();
+            editButton=new Button("E");
+            deleteButton=new Button("D");
+            //edit the size of buttons/icons
+            editButton.setPrefSize(12, 12);
+            deleteButton.setPrefSize(25, 12);
+
+            editButton.setOnAction(event -> handleEdit(getItem()));
+            deleteButton.setOnAction(event -> handleDelete(getItem()));
+
+            // Add components to the element (the line from the list view)
+            container.getChildren().addAll(deleteButton,editButton,tagLabel);
+            container.setHgrow(tagLabel, Priority.ALWAYS);
+
+            // Set spacing between components, 4 it is good
+            container.setSpacing(4);
+
+            // Set padding between lines
+            container.setPadding(new javafx.geometry.Insets(1));
+        }
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty || item == null) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                tagLabel.setText(item);
+                setGraphic(container);
+            }
+        }
+    }
+    private void handleEdit(String tag) {
+        // Here we would edit the tag
+        System.out.println("Edit: " + tag);
+    }
+
+    // Method to handle delete button click
+    private void handleDelete(String tag) {
+        // Here we would delete the tag
+        System.out.println("Delete: " + tag);
+    }
+
+    /**
+     * This is like a back button
+     * @param e the action
+     */
     public void exitPage(ActionEvent e){
-        System.out.println("closed DebtsCtrl");
+        System.out.println("closed StatsCtrl");
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         mainCtrl.initialize(stage, EventPageCtrl.getPair(), EventPageCtrl.getTitle());
     }
