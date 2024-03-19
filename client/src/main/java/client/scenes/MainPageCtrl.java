ef3dbe30aa5006b5944fe54b0fd2751cd2fa56a6
Ivan Markov <i.k.markov@student.tudelft.nl>
2024-03-19 19:26:33 +0100

    added the front end of flipping the flag when pressed

--- client/src/main/java/client/scenes/MainPageCtrl.java
+++ client/src/main/java/client/scenes/MainPageCtrl.java
@@ -6,21 +6,32 @@ import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import commons.Event;
+
+
+import javafx.animation.*;
 import javafx.event.ActionEvent;
+
+
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.util.Pair;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import static com.google.inject.Guice.createInjector;
+import static javafx.animation.Interpolator.*;
 
 public class MainPageCtrl implements Controller, Initializable {
 
@@ -29,9 +40,9 @@ public class MainPageCtrl implements Controller, Initializable {
   @FXML
   private TextField joinInput;
   @FXML
-  private ImageView flagImage;
-  @FXML
   private ListView<EventHelper> recentList;
+  @FXML
+  private Button flagButton;
 
   private EventHelper selectedEv;
   //Imports used to swap scenes
@@ -41,6 +52,13 @@ public class MainPageCtrl implements Controller, Initializable {
 
   private static final MainCtrl mainCtrl = INJECTOR.getInstance(MainCtrl.class);
   private ServerUtils server;
+
+  private Locale currentLocale = new Locale("en", "US"); // Default to English
+
+  private int counter = 0;
+  private TranslateTransition  smoothShake;
+  private SequentialTransition seqTransition;
+
   @Inject
   public MainPageCtrl(ServerUtils server){
     this.server = server;
@@ -93,8 +111,72 @@ public class MainPageCtrl implements Controller, Initializable {
         System.out.println(e);
       }
     });
+
+    putFlag("enFlag.png");
+    prepareAnimation();
+
+    flagButton.setOnMouseClicked(event -> {
+
+        try {
+            Thread.sleep(150);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        changeFlag();
+    });
+  }
+  public void changeFlag(){
+    seqTransition.play();
+    if(currentLocale.getLanguage().equals("nl")){
+      System.out.println("in en handle");
+      currentLocale = new Locale("en", "US");
+      PauseTransition pause = new PauseTransition(Duration.millis(150));
+      pause.setOnFinished(e -> putFlag("enFlag.png")); // This executes changeFlag after the pause
+      pause.play();
+
+    }
+    else{
+      System.out.println("in nl handle");
+      currentLocale = new Locale("nl", "NL");
+      PauseTransition pause = new PauseTransition(Duration.millis(150));
+      pause.setOnFinished(e -> putFlag("nlFlag.png")); // This executes changeFlag after the pause
+      pause.play();
+
+    }
+  }
+  public void toggleLanguage(){
+      System.out.println("image pressed " + counter++);
   }
 
+  public void putFlag(String path){
+    Image image = new Image(path);
+    BackgroundSize backgroundSize =
+            new BackgroundSize(100, 100, true, true, true, false);
+    BackgroundImage backgroundImage = new BackgroundImage(image,
+            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+            backgroundSize);
+
+    Background background = new Background(backgroundImage);
+
+    flagButton.setBackground(background);
+  }
+
+  public void prepareAnimation(){
+    // Shrink transition
+    ScaleTransition shrink = new ScaleTransition(Duration.millis(150), flagButton);
+    shrink.setToY(0.0); // Shrink to disappear on the Y axis
+    shrink.setInterpolator(Interpolator.EASE_BOTH);
+
+    ScaleTransition restore = new ScaleTransition(Duration.millis(150), flagButton);
+    restore.setToY(1); // Restore to original size on the Y axis
+    restore.setInterpolator(Interpolator.EASE_BOTH);
+
+
+
+    seqTransition = new SequentialTransition(shrink, restore);
+
+    flagButton.setOnMouseClicked(event -> seqTransition.play());
+  }
   public static Pair<Controller, Parent> getPair() {
     return FXML.load(Controller.class, "client", "scenes", "mainPage.fxml");
   }
