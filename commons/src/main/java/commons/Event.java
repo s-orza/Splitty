9e323985d4b7378b8d2d7b8a37f05e13d2bcaf43
Olav Peters <o.c.peters@student.tudelft.nl>
2024-03-22 21:01:09 +0100

    transferred all functions and changed appropriately

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -109,6 +109,277 @@ public class Event {
 //        }
 //    }
 
+
+    
+    // Debt Functions
+
+    /**
+     * adds a new debt if it does not exist yet.
+     * If a debt exists between the creditor and debtor, it modifies this debt accordingly
+     * @param amount the amount of the debt
+     * @param currency the currency of the debt
+     * @param debtor the debtor who owes
+     * @param creditor the creditor who is owed
+     * @return the debt that was modified or created
+     */
+    public Debt addDebt(double amount, String currency, Participant debtor, Participant creditor){
+        long d = debtor.getParticipantID();
+        long c = creditor.getParticipantID();
+        // if a debt already exists, modify it
+        for (Debt debt: debts) {
+            if(debt.getDebtor()==d && debt.getCreditor()==c){
+                // if diff currency, return null (add currency conversion later)
+                if(!Objects.equals(currency, debt.getCurrency())){return null;}
+
+                //add debts
+                debt.setAmount(debt.getAmount() + amount);
+                return debt;
+            }
+            else if(debt.getDebtor()==c && debt.getCreditor()==d){
+                // if diff currency, return null (add currency conversion later)
+                if(!Objects.equals(currency, debt.getCurrency())){return null;}
+
+                //subtract debts
+                debt.setAmount(debt.getAmount() - amount);
+
+                //if cancel out
+                if(debt.getAmount() == 0){return (settleDebt(debt));}
+                //if cred & debt switch
+                if(debt.getAmount() < 0){
+                    long oldDebtor = debt.getDebtor();
+                    debt.setDebtor(debt.getCreditor());
+                    debt.setCreditor(oldDebtor);
+                    debt.setAmount(Math.abs(debt.getAmount()));
+                }
+                return debt;
+            }
+        }
+        Debt res = new Debt(amount, currency, debtor, creditor);
+        debts.add(res);
+        return res;
+    }
+
+    /**
+     * adds a new debt if it does not exist yet.
+     * If a debt exists between the creditor and debtor, it modifies this debt accordingly
+     * @param amount the amount of the debt
+     * @param currency the currency of the debt
+     * @param debtorID the ID of the debtor who owes
+     * @param creditorID the ID of the creditor who is owed
+     * @return the debt that was modified or created
+     */
+    public Debt addDebt(double amount, String currency, long debtorID, long creditorID){
+        // if a debt already exists, modify it
+        for (Debt debt: debts) {
+            if(debt.getDebtor()==debtorID && debt.getCreditor()==creditorID){
+                // if diff currency, return null (add currency conversion later)
+                if(!Objects.equals(currency, debt.getCurrency())){return null;}
+
+                //add debts
+                debt.setAmount(debt.getAmount() + amount);
+                return debt;
+            }
+            else if(debt.getDebtor()==creditorID && debt.getCreditor()==debtorID){
+                // if diff currency, return null (add currency conversion later)
+                if(!Objects.equals(currency, debt.getCurrency())){return null;}
+
+                //subtract debts
+                debt.setAmount(debt.getAmount() - amount);
+
+                //if cancel out
+                if(debt.getAmount() == 0){return (settleDebt(debt));}
+                //if cred & debt switch
+                if(debt.getAmount() < 0){
+                    long oldDebtor = debt.getDebtor();
+                    debt.setDebtor(debt.getCreditor());
+                    debt.setCreditor(oldDebtor);
+                    debt.setAmount(Math.abs(debt.getAmount()));
+                }
+                return debt;
+            }
+        }
+        Debt res = new Debt(amount, currency, debtorID, creditorID);
+        debts.add(res);
+        return res;
+    }
+
+
+    /**
+     * adds a new debt if it does not exist yet.
+     * If a debt exists between the creditor and debtor, it modifies this debt accordingly
+     * @param d the debt to be added
+     * @return the debt that was modified or added
+     */
+    public Debt addDebt(Debt d){
+        // if a debt already exists, modify it
+        for (Debt debt: debts) {
+            if(debt.getDebtor()==d.getDebtor() && debt.getCreditor()==d.getCreditor()){
+                // if diff currency, return null (add currency conversion later)
+                if(!Objects.equals(d.getCurrency(), debt.getCurrency())){return null;}
+
+                //add debts
+                debt.setAmount(debt.getAmount() + d.getAmount());
+                return debt;
+            }
+            else if(debt.getDebtor()==d.getCreditor() && debt.getCreditor()==d.getDebtor()){
+                // if diff currency, return null (add currency conversion later)
+                if(!Objects.equals(d.getCurrency(), debt.getCurrency())){return null;}
+
+                //subtract debts
+                debt.setAmount(debt.getAmount() - d.getAmount());
+
+                //if cancel out
+                if(debt.getAmount() == 0){return (settleDebt(debt));}
+                //if cred & debt switch
+                if(debt.getAmount() < 0){
+                    long oldDebtor = debt.getDebtor();
+                    debt.setDebtor(debt.getCreditor());
+                    debt.setCreditor(oldDebtor);
+                    debt.setAmount(Math.abs(debt.getAmount()));
+                }
+                return debt;
+            }
+        }
+        debts.add(d);
+        return d;
+    }
+
+    /**
+     * settles a debt by removing it
+     * @param debt debt to be settled
+     * @return the settled debt, or null if the debt does not exist
+     */
+    public Debt settleDebt(Debt debt){
+        if(!debts.remove(debt)){
+            return null;
+        }
+        return debt;
+    }
+
+    /**
+     * Resets all the debts by removing them
+     * @return the list of previous debts that was removed
+     */
+    public List<Debt> clearAllDebts(){
+        ArrayList<Debt> result = (ArrayList) getDebts();
+        setDebts(new ArrayList<Debt>());
+        return result;
+    }
+
+    /**
+     * returns a list of all the debts a participant has
+     * @param debtor the participant who owes
+     * @return List of debts a participant owes
+     */
+    public List<Debt> getDebts(Participant debtor){
+        long d = debtor.getParticipantID();
+        ArrayList<Debt> result = new ArrayList<>();
+        for (Debt debt: debts) {
+            if(debt.getDebtor()==d){
+                result.add(debt);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * returns a list of all the debts a participant has
+     * @param debtor the participant who owes
+     * @return List of debts a participant owes
+     */
+    public ArrayList<Debt> getDebts(long debtor){
+        ArrayList<Debt> result = new ArrayList<>();
+        for (Debt debt: debts) {
+            if(debt.getDebtor()==debtor){
+                result.add(debt);
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * returns a list of all the credits a participant is owed
+     * @param creditor the participant who is owed
+     * @return ArrayList of credits a participant is owed
+     */
+    public ArrayList<Debt> getCredits(Participant creditor){
+        long c = creditor.getParticipantID();
+        ArrayList<Debt> result = new ArrayList<>();
+        for (Debt credit: debts) {
+            if(credit.getCreditor()==c){
+                result.add(credit);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * returns a list of all the credits a participant is owed
+     * @param creditor the participant who is owed
+     * @return ArrayList of credits a participant is owed
+     */
+    public ArrayList<Debt> getCredits(long creditor){
+        ArrayList<Debt> result = new ArrayList<>();
+        for (Debt credit: debts) {
+            if(credit.getCreditor()==creditor){
+                result.add(credit);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * returns a list of all the credits and debts a participant is involved in
+     * @param participant the participant
+     * @return ArrayList of debts the participant is involved in
+     */
+    public ArrayList<Debt> getDebtsAndCredits(Participant participant){
+        long p = participant.getParticipantID();
+        ArrayList<Debt> result = new ArrayList<>();
+        for (Debt debt: debts) {
+            if(debt.getCreditor()==p || debt.getDebtor()==p){
+                result.add(debt);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * returns a list of all the credits and debts a participant is involved in
+     * @param participantID the participant's ID
+     * @return ArrayList of debts the participant is involved in
+     */
+    public ArrayList<Debt> getDebtsAndCredits(long participantID){
+        ArrayList<Debt> result = new ArrayList<>();
+        for (Debt debt: debts) {
+            if(debt.getCreditor()==participantID || debt.getDebtor()==participantID){
+                result.add(debt);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * The getter method for the debts
+     *
+     * @return debts of this event
+     **/
+    public List<Debt> getDebts() {
+        return debts;
+    }
+
+    /**
+     * The setter method for the debts
+     *
+     * @param debts The List of Debts to set debts to
+     **/
+    public void setDebts(List<Debt> debts) {
+        this.debts = debts;
+    }
+
+    // End Debt Functions
+
     public void setEventId(long eventID) {
         this.eventID = eventID;
     }
@@ -170,24 +441,6 @@ public class Event {
 
     public Date getCreationDate() {
         return creationDate;
-    }
-
-    /**
-     * The getter method for the debts
-     *
-     * @return debts of this event
-     **/
-    public List<Debt> getDebts() {
-        return debts;
-    }
-
-    /**
-     * The setter method for the debts
-     *
-     * @param debts The List of Debts to set debts to
-     **/
-    public void setDebts(List<Debt> debts) {
-        this.debts = debts;
     }
 
     @Override
