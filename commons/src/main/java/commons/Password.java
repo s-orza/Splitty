d61534dcb38ca914d3a8c215e07b602928058718
Olav Peters <o.c.peters@student.tudelft.nl>
2024-03-29 17:35:55 +0100

    created generate password method

--- commons/src/main/java/commons/Password.java
+++ commons/src/main/java/commons/Password.java
@@ -11,15 +11,87 @@ public class Password {
     @Id
     private long debtID;
 
+    private int length = 7;
     private String password;
 
     public Password() {
-
         password = genPass();
 
     }
 
+    public Password(int length) {
+        this.length = length;
+        password = genPass();
+
+    }
+
+    /**
+     * generates a random String password
+     * @return a random String
+     */
     private String genPass(){
-        return "";
+        String chars =  "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789";
+        StringBuilder pass = new StringBuilder();
+        Random r = new Random();
+
+        for(int i=0;i<length;i++){
+            pass.append(chars.charAt(r.nextInt(chars.length())));
+        }
+
+        return pass.toString();
+    }
+
+    /**
+     * The getter method for the debtID attribute
+     *
+     * @return debtID of this object
+     **/
+    public long getDebtID() {
+        return debtID;
+    }
+
+    /**
+     * The setter method for the debtID attribute
+     *
+     * @param debtID The value to set debtID to
+     **/
+    public void setDebtID(long debtID) {
+        this.debtID = debtID;
+    }
+
+    /**
+     * The getter method for the length attribute
+     *
+     * @return length of this object
+     **/
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * The setter method for the length attribute
+     *
+     * @param length The value to set length to
+     **/
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    /**
+     * The getter method for the password attribute
+     *
+     * @return password of this object
+     **/
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * The setter method for the password attribute
+     *
+     * @param password The value to set password to
+     **/
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
