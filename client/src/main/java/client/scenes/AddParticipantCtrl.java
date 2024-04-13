5255479e4f8835ee54f601dc93d8da0380b765c4
Pop David Gabriel <d.g.pop@student.tudelft.nl>
2024-04-13 23:06:00 +0200

    Missed checkstyle error. Fixed!

--- client/src/main/java/client/scenes/AddParticipantCtrl.java
+++ client/src/main/java/client/scenes/AddParticipantCtrl.java
@@ -17,7 +17,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
